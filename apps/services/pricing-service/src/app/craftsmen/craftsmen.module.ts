9bfeeabb6ee0d587343f5dbd8937ab0148a85584
Kamgais <cyrilkamgais1203@gmail.com>
2026-05-26 14:06:00 +0200

    Fix CraftsmenModule dependency and CORS config

--- apps/services/pricing-service/src/app/craftsmen/craftsmen.module.ts
+++ apps/services/pricing-service/src/app/craftsmen/craftsmen.module.ts
@@ -4,9 +4,13 @@ import { Craftsman } from './entities/craftsman.entity';
 import { CraftsmanTradeAssignment } from './entities/craftsman-trade-assignment.entity';
 import { CraftsmenService } from './craftsmen.service';
 import { CraftsmenController } from './craftsmen.controller';
+import { PricingCatalogsModule } from '../pricing-catalogs/pricing-catalogs.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Craftsman, CraftsmanTradeAssignment])],
+  imports: [
+    TypeOrmModule.forFeature([Craftsman, CraftsmanTradeAssignment]),
+    PricingCatalogsModule,
+  ],
   providers: [CraftsmenService],
   controllers: [CraftsmenController],
   exports: [CraftsmenService],
